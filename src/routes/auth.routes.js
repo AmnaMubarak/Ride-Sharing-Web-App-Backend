348e8a674b892925d107dba1e6745e045ecc8a9c
AmnaMubarak <amnamubarakk111@gmail.com>
2025-02-10 01:26:06 +0500

    add caching middleware to user retrieval route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { register, login, getMe } = require('../controllers/auth.controller');
 const { protect } = require('../middleware/auth');
+const { cache } = require('../middleware/cache');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', protect, getMe);
+router.get('/me', protect, cache(300), getMe);
 
 module.exports = router; 
